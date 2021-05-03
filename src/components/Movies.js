476a42ce1b3fb480f6c8ebcfa86cc8a92abe9b5b
luzybry94 <luzybry94@gmail.com>
2021-05-03 14:49:27 -0700

    commit tree b1fc2f971b19e0eec11d89a87306fb84a4bcc3c5

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+            <div>
+              <h3>Name: {movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <p>Genres: <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul></p>
+            </div>
+          )}
+        )
+      }
     </div>
   );
 };
